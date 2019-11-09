ad787bfb2181ab37b94842ab6e72d66611eeff6b
elias roussos <eliasisaiah@outlook.com>
2019-11-09 12:17:43 -0500

    -it is technically working. make sure you have a big screen or prepare to zoom out a bit

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -11,25 +11,53 @@ class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends,
-    previousID: null,
+    previousIDs: [0],
     score: 0,
     topScore: 0,
   };
 
+  
+  restart = () => {
+    alert("game over");
+    this.setState({
+      friends,
+      previousIDs: [0],
+      score: 0,
+      topScore: this.state.topScore,
+    })
+    
+    console.log("this.state endgame", this.state)
+  }
+  
   incrementScore = () => {
-    const newScore = this.state.score++;
-    this.setState({...this.state, score:newScore} );
-    console.log("this.state.score", this.state.score);
+    
+    let newScore = this.state.score + 1;
+
+    const newTopScore = newScore > this.state.topScore ? newScore : this.state.topScore;
+
+    if (newScore > 8) {
+      alert("congrats you won.");
+      this.restart();
+    }
+
+    return {
+      newScore,
+      newTopScore
+    }
   }
 
   shuffleFriends = (id: number) => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends: Friend[] = shuffle(this.state.friends);
-    this.incrementScore()
 
-    // const friends:Friend[] = this.state.friends.filter((friend:Friend) => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ ...this.state, friends });
+    if(!this.state.previousIDs.includes(id)) {
+
+      const newScores = this.incrementScore();
+      
+      this.setState({ friends, previousIDs: [...this.state.previousIDs, id], score:newScores.newScore, topScore:newScores.newTopScore });
+    } 
+    else {
+      return this.restart()
+    };
   };
 
 
@@ -39,7 +67,6 @@ class App extends Component {
       <>
       <Navbar score={this.state.score} topScore={this.state.topScore} />
       <Wrapper>
-        <Title>Friends List</Title>
         {this.state.friends.map(friend => (
           <FriendCard
             shuffleFriends={this.shuffleFriends}
